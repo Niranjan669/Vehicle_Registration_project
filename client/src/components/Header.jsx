fba243ca40b87761271034a26fc345866b0ca3bb
Niranjan <niranjanbalurgi@gmail.com>
2025-09-01 16:47:08 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,1 +1,25 @@
-header
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">VehicleApp</Link>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/add">Add Vehicle</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/view">View Vehicles</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+export default Header;
