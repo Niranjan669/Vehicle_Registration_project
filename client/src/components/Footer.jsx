22dde355f7201808dd39ed0bc5f80f93576f7282
Niranjan <niranjanbalurgi@gmail.com>
2025-09-01 16:47:40 +0530

    Create Footer.jsx

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <footer className="bg-dark text-light text-center p-3 mt-4">
+      <p>© 2025 Vehicle Management App</p>
+    </footer>
+  );
+}
+export default Footer;
