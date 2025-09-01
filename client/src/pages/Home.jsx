6ce3bbdd2d7c85ca909b397fb2adca60fadab1d5
Niranjan <niranjanbalurgi@gmail.com>
2025-09-01 16:42:33 +0530

    Create Home.jsx

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="container mt-4">
+      <h2>Welcome to Vehicle Management System</h2>
+      <p>Manage vehicles and users with CRUD operations easily.</p>
+    </div>
+  );
+}
+
+export default Home;
